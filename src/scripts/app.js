ecdbd133a5ab57f48839edfbb7fc129266a5fd77
LeToz <sahan.toksoz@student.heaj.be>
2026-06-10 03:12:00 +0200

    Ajout anim GSAP textplugin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,23 @@
 "use strict";
 
+gsap.registerPlugin(TextPlugin);
+
+  gsap.set(".terminal-list", { autoAlpha: 0 });
+
+  const tl = gsap.timeline();
+
+  const mainElements = document.querySelectorAll(".terminal-text");
   
+  mainElements.forEach((element) => {
+    const originalContent = element.innerHTML;
+    element.innerHTML = "";
+    const typingSpeed = originalContent.length * 0.010;
+
+    tl.to(element, {
+      text: { value: originalContent },
+      duration: typingSpeed,
+      ease: "none"
+    });
+  });
+
+  tl.set(".terminal-list", { autoAlpha: 1 });
